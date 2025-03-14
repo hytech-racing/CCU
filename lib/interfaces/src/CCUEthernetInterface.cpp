da4a3faeaf3e9829a027d07174e2b3f07f14646e
buip <nazcar101@gmail.com>
2025-03-14 11:23:12 -0400

    ethernet interface updated with comments

--- lib/interfaces/src/CCUEthernetInterface.cpp
+++ lib/interfaces/src/CCUEthernetInterface.cpp
@@ -4,15 +4,12 @@
 
 
 
-//This is the start of ethernet interface function not implemented yet
-
-
 //take the values from the protobuf message and update CCUInput_s 
 
 void CCUEthernetInterface::recieve_pb_msg_acu_all_data(const hytech_msgs_ACUAllData_s &msg_in, ACUAllData_s &acu_all_data){
 
-    for (uint32_t i = 0; i < msg_in.voltages_count; ++i){
-        acu_all_data.voltages[i] = msg_in.voltages[i];
+    for (uint32_t i = 0; i < msg_in.voltages_count; ++i){ //protoc struct is an array of floats
+        acu_all_data.voltages[i] = msg_in.voltages[i]; //should be passed in as normal voltage based on vcr
     }
     for (uint32_t i = 0; i < msg_in.cell_temperatures_count; ++i){
         acu_all_data.cell_temperatures[i] = msg_in.cell_temperatures[i];
