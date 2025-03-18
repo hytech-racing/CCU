c57026b4dbf5afb987b2f9011f998ee96bd9f407
buip <nazcar101@gmail.com>
2025-03-17 23:42:06 -0400

    calculate_charge_current outputs a single float with the correct charge current

--- lib/systems/include/MainChargeSystem.h
+++ lib/systems/include/MainChargeSystem.h
@@ -2,11 +2,11 @@
 #define __MAINCHARGE_H__
 
 
-#include "CCUEthernetInterface.h"
 #include "CCUParams.h"
+#include "CCUEthernetInterface.h"
 
 namespace MainChargeSystem{
-  void calculate_charge_current(ACUAllData_s inputValues, CCUParams& chargeParams, CCUOutput_s &chargeOutputs); 
+  float calculate_charge_current(ACUAllData_s inputValues, CCUParams& chargeParams); 
 }
 
 #endif
