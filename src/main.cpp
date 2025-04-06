0b522dd2c8b6cfbb8b4d2e49431428c68ef846e6
mansi-b29 <133283027+mansi-b29@users.noreply.github.com>
2025-04-06 01:03:25 -0400

    added all working can code to main

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,10 @@
 #include "CCUCANInterfaceImpl.h"
 #include "CCUTasks.h"
 
+
+FlexCAN_Type<CAN2> CHARGER_CAN; // gets defined in main as of right now
+FlexCAN_Type<CAN1> ACU_CAN;
+
 /* Parameters */
 ACUAllData_s acu_all_data;
 CCUParams ccu_params; 
@@ -30,42 +34,51 @@ qn::EthernetUDP udp; //setup of qn namespace
 /* Scheduler Setup */
 HT_SCHED::Scheduler& scheduler = HT_SCHED::Scheduler::getInstance(); 
 
+etl::delegate<void(CANInterfaces &, const CAN_message_t &, unsigned long)> main_can_recv = etl::delegate<void(CANInterfaces &, const CAN_message_t &, unsigned long)>::create<CCUCANInterfaceImpl::ccu_CAN_recv>();
+
+
 
 /* Task Declarations */
 HT_TASK::Task update_display_task(HT_TASK::DUMMY_FUNCTION, run_update_display_task, CCUConstants::UPDATE_DISPLAY_PRIORITY, CCUConstants::HT_SCHED_PERIOD_US); //not sure what the priorities and loop rate should be for each task
 HT_TASK::Task read_dial_task(HT_TASK::DUMMY_FUNCTION, run_read_dial_task, CCUConstants::READ_DIAL_PRIORITY, CCUConstants::HT_SCHED_PERIOD_US);
-HT_TASK::Task send_ACU_CAN(HT_TASK::DUMMY_FUNCTION, handle_enqueue_acu_can_data, CCUConstants::ENQUEUE_ACU_CAN_DATA_PRIORITY, CCUConstants::HT_SCHED_PERIOD_US);
-HT_TASK::Task send_Charger_CAN(HT_TASK::DUMMY_FUNCTION, handle_enqueue_charger_can_data, CCUConstants::ENQUEUE_CHARGER_CAN_DATA_PRIORITY, CCUConstants::HT_SCHED_PERIOD_US);
+HT_TASK::Task queue_ACU_CAN(HT_TASK::DUMMY_FUNCTION, handle_enqueue_acu_can_data, 1, 100000UL);
+HT_TASK::Task queue_Charger_CAN(HT_TASK::DUMMY_FUNCTION, handle_enqueue_charger_can_data, 1, 100000UL);
 HT_TASK::Task send_ethernet(HT_TASK::DUMMY_FUNCTION, run_send_ethernet, CCUConstants::SEND_ETHERNET_PRIORITY, CCUConstants::HT_SCHED_PERIOD_US);
 HT_TASK::Task receive_ethernet(HT_TASK::DUMMY_FUNCTION, run_receive_ethernet, CCUConstants::RECIEVE_ETHERNET_PRIORITY, CCUConstants::HT_SCHED_PERIOD_US);
-HT_TASK::Task send_all_data(HT_TASK::DUMMY_FUNCTION, handle_send_all_data, CCUConstants::SEND_ALL_DATA_PRIORITY, CCUConstants::HT_SCHED_PERIOD_US);
-
+HT_TASK::Task send_all_data(HT_TASK::DUMMY_FUNCTION, handle_send_all_data, 2);
+HT_TASK::Task run_sample_CAN_data(HT_TASK::DUMMY_FUNCTION, sample_CAN_data, 2);
 
  
-/* Extern CAN instances */
+/* Extern CAN instances 
 FlexCAN_T4<CAN1> ACU_CAN;
-FlexCAN_T4<CAN2> CHARGER_CAN;
+FlexCAN_T4<CAN2> CHARGER_CAN; */
 
 
 /* Functions */
 void setup() {
   Serial.begin(115200); //I dont know if this is the baudrate I also don't know if this is even needed
+  delay(4000);
   qn::Ethernet.begin(); //begins QNEthernet
 
 
-  ACUInterfaceInstance::instance();
-  ChargerInterfaceInstance::instance();
+ // ACUInterfaceInstance::instance();
+  //ChargerInterfaceInstance::instance();
 
-  //scheduler.schedule(update_display_task);
-  //scheduler.schedule(read_dial_task);
-  scheduler.schedule(send_ACU_CAN);
-  scheduler.schedule(send_Charger_CAN);
-  //scheduler.schedule(send_ethernet);
-  //scheduler.schedule(receive_ethernet);
+  intitialize_all_interfaces();
+  scheduler.schedule(update_display_task);
+  scheduler.schedule(read_dial_task);
+  scheduler.schedule(queue_ACU_CAN);
+  scheduler.schedule(queue_Charger_CAN);
+  scheduler.schedule(send_ethernet);
+  scheduler.schedule(receive_ethernet);
   scheduler.schedule(send_all_data);
+
+  handle_CAN_setup(ACU_CAN, CCUConstants::CAN_BAUDRATE, &CCUCANInterfaceImpl::on_acu_can_receive);
+  handle_CAN_setup(CHARGER_CAN, CCUConstants::CAN_BAUDRATE, &CCUCANInterfaceImpl::on_charger_can_receive);
   
 }
 
 void loop() {
   scheduler.run();
+  delay(10);
 }
