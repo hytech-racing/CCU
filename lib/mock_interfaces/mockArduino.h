e4149a18593b4b26d23a6a7051e77a769e4342ee
mansi-b29 <133283027+mansi-b29@users.noreply.github.com>
2025-05-27 16:09:09 -0400

    attempt two

--- lib/mock_interfaces/mockArduino.h
+++ lib/mock_interfaces/mockArduino.h
@@ -6,9 +6,9 @@
 #define INPUT 0
 #define OUTPUT 1
 
-inline void pinMode(int pin, int mode) {}
-inline void digitalWrite(int pin, int value) {}
-inline int digitalRead(int pin) {return HIGH;}
+inline void pinMode(int pin, int mode) {} //NOLINT (used as a mock for test_systems_env)
+inline void digitalWrite(int pin, int value) {} //NOLINT (used as a mock for test_systems_env)
+inline int digitalRead(int pin) {return HIGH;} //NOLINT (used as a mock for test_systems_env)
 
 
 
