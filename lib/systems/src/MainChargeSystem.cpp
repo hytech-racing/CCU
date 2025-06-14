4b09c19075e26590dcfdef9a7cb9f77eaccca397
mansi-b29 <133283027+mansi-b29@users.noreply.github.com>
2025-06-14 17:31:34 -0400

    added logic to stop charging when ACU shutdown goes low

--- lib/systems/src/MainChargeSystem.cpp
+++ lib/systems/src/MainChargeSystem.cpp
@@ -20,10 +20,15 @@ void MainChargeSystem::calculate_charge_current() {
   high_voltage = ACUInterfaceInstance::instance().get_latest_data().high_voltage; //the highest voltage in any of the cells
   total_voltage = ACUInterfaceInstance::instance().get_latest_data().total_voltage; //the total voltage in the pack
 
-  bool shutdown_low = (digitalRead(_ccu_data.SHDN_E_READ) != HIGH);
+  bool shutdown_low = (digitalRead(_ccu_data.SHDN_E_READ) != HIGH); //e-stop on charge cart
+
+  /** acu_state comes from the bms_status message. If shutdown is low on ACU (HVP is unplugged), acu_state = 1.
+   *  If acu_state = 2, we should/are safe to be charging
+   */
+  bool acu_shutdown_low = ACUInterfaceInstance::instance().get_latest_data().acu_state == 1; 
   
   bool voltage_reached = (high_voltage >= _ccu_data.cutoff_voltage) || (ACUInterfaceInstance::instance().get_latest_data().total_voltage > _ccu_data.max_pack_voltage);
-  if (shutdown_low)
+  if (shutdown_low || acu_shutdown_low)
   {
     _ccu_data.charging_state = ChargingState_e::NOT_CHARGING;
   } else if(voltage_reached)
@@ -35,14 +40,14 @@ void MainChargeSystem::calculate_charge_current() {
   
 
   /* Tells the charger to stop charging if the shutdown button is pressed or one of the cell voltags is too high */
-  if (voltage_reached || shutdown_low) {  //ACU will cause a BMS fault if there is a cell or board temp that is too high
+  if (voltage_reached || shutdown_low || acu_shutdown_low) {  //ACU will cause a BMS fault if there is a cell or board temp that is too high
     _ccu_data.calculated_charge_current = 0;
     _ccu_data.balancing_enabled = false;
   } else {
     if (low_voltage < _ccu_data.balancing_voltage) {
       _ccu_data.calculated_charge_current = _ccu_data.safe_charging_current; //15 - safe charging current to be at if not cell balancing
     } else {
-      _ccu_data.calculated_charge_current = _ccu_data.charger_current_max; //30 as of now
+      _ccu_data.calculated_charge_current = _ccu_data.charger_current_max; //120
     }
   } 
 }
