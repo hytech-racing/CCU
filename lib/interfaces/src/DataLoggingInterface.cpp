f24a9f059677b100b4eb3ffb00921880d84184dc
SoorajTharumia <sendsooraj@gmail.com>
2025-10-26 19:09:23 -0400

    fixed data closing issue

--- lib/interfaces/src/DataLoggingInterface.cpp
+++ lib/interfaces/src/DataLoggingInterface.cpp
@@ -15,17 +15,14 @@ bool DataLoggingInterface::init() {
     }
     if (data_file.size() == 0) {
         data_file.println("timestamp,pack_current,pack_voltage,cell_voltage_avg,cell_voltage_min,cell_voltage_max,cell_temp_min,cell_temp_max,board_temp_max,charging_state");
-        data_file.flush();
     }
+
+    data_file.close();
 
     return true;
 }
 
 void DataLoggingInterface::log_data() {
-    if (!data_file) {
-        return;
-    }
-
     auto acu_data = ACUInterfaceInstance::instance().get_latest_data();
     auto em_data = EnergyMeterInterfaceInstance::instance().get_latest_em_data();
 
@@ -55,5 +52,5 @@ void DataLoggingInterface::log_data() {
     data_file.print(static_cast<int>(ChargerStateMachineInstance::instance().get_state()));
     data_file.println();
 
-    data_file.flush();
+    data_file.close();
 }
