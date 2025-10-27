9ca9ac303dc9b146c36212c5036d6eff74e30423
Shyiu <mightydragon334@gmail.com>
2025-10-27 14:45:54 -0400

    Removed unused code to reflect new CCU logic changes

--- lib/systems/src/MainChargeSystem.cpp
+++ lib/systems/src/MainChargeSystem.cpp
@@ -5,14 +5,9 @@
 
 void MainChargeSystem::calculate_charge_current() {
 
-  float average_voltage = 0;
-  float low_voltage = 0;
   float high_voltage = 0;
   float total_voltage = 0;
-  float calculated_charge_current = 0; 
 
-  average_voltage = ACUInterfaceInstance::instance().get_latest_data().average_voltage; //average voltage across the cells
-  low_voltage = ACUInterfaceInstance::instance().get_latest_data().low_voltage; //the lowest voltage in any of the cells
   high_voltage = ACUInterfaceInstance::instance().get_latest_data().high_voltage; //the highest voltage in any of the cells
   total_voltage = ACUInterfaceInstance::instance().get_latest_data().total_voltage; //the total voltage in the pack
 
@@ -23,7 +18,7 @@ void MainChargeSystem::calculate_charge_current() {
    */
   bool acu_shutdown_low = ACUInterfaceInstance::instance().get_latest_data().acu_state == 1; //NOLINT
   
-  bool voltage_reached = (high_voltage >= _ccu_data.cutoff_voltage) || (ACUInterfaceInstance::instance().get_latest_data().total_voltage > _ccu_data.max_pack_voltage); //NOLINT
+  bool voltage_reached = (high_voltage >= _ccu_data.cutoff_voltage) || (total_voltage > _ccu_data.max_pack_voltage); //NOLINT
   if (shutdown_low || acu_shutdown_low)
   {
     _ccu_data.charging_state = ChargingState_e::NOT_CHARGING;
@@ -33,7 +28,6 @@ void MainChargeSystem::calculate_charge_current() {
   } else {
     _ccu_data.charging_state = ChargingState_e::CHARGING;
   }
-  
 
   /* Tells the charger to stop charging if the shutdown button is pressed or one of the cell voltags is too high */
   if (voltage_reached || shutdown_low || acu_shutdown_low) {  //ACU will cause a BMS fault if there is a cell or board temp that is too high
