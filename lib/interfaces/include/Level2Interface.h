afcfdd9e97d769e9f9e12745f1c57b33881d40f3
Vansh Aggarwal <66187835+vanshagg233@users.noreply.github.com>
2026-01-25 18:24:30 -0600

    added 2 functions to new interface

--- lib/interfaces/include/Level2Interface.h
+++ lib/interfaces/include/Level2Interface.h
@@ -14,6 +14,7 @@ namespace LEVEL2_PARAMS {
     constexpr const pin CONTROL_V_SENSE = 37; //pin to read voltage on CP line
     constexpr const pin CONTROL_PWM_SENSE = 29; // pin to read PWM signal from CP
     constexpr const pin PROXIMITY_SENSE = 15; // pin to read voltage on PP
+    constexpr const pin START_CHARGE = 32; //pin used to control start charging fet
 
 };
 
@@ -23,18 +24,28 @@ private:
     pin teensy_cp_v;
     pin teensy_cp_pwm;
     pin teensy_pp_v;
-    pin enabled;
+    bool enabled;
+    pin teensy_start_charge;
+
+    constexpr float ADC_REF_VOLTAGE = 3.3f;
+    constexpr float ADC_MAX_COUNT   = 4095.0f;
 
 public:
     Level2Interface(
         pin cp_v = LEVEL2_PARAMS::CONTROL_V_SENSE,
         pin cp_pwm = LEVEL2_PARAMS::CONTROL_PWM_SENSE,
-        pin pp_v = LEVEL2_PARAMS::PROXIMITY_SENSE) :
+        pin pp_v = LEVEL2_PARAMS::PROXIMITY_SENSE,
+        pin sc = LEVEL2_PARAMS::START_CHARGE) :
             teensy_cp_v(cp_v),
             teensy_cp_pwm(cp_pwm),
             teensy_pp_v(pp_v),
-            enabled(CCUData.level_2_enabled)
+            enabled(CCUData.level_2_enabled),
+            teensy_start_charge(sc)
     {};
     
     void init();
+    void toggle_240_charging();
+    void check_charge_condition();
+
+
 }
--- lib/interfaces/src/Level2Interface.cpp
+++ lib/interfaces/src/Level2Interface.cpp
@@ -1,5 +1,33 @@
 #include "Level2Interface.h"
 
 void Level2Interface::init() {
-    pinMode(CONTROL_V_SENSE, in)
+    pinMode(teensy_cp_v, INPUT);
+    pinMode(teensy_cp_pwm, INPUT);
+    pinMode(teensy_pp_v, INPUT);
+    pinMode(teensy_start_charge, OUTPUT);
+    digitalWrite(teensy_start_charge, LOW);
+    analogReadResolution(12);
+}
+
+void toggle_240_charging() {
+    if (enabled) {
+        digitalWrite(teensy_start_charge, HIGH);
+    } 
+    else 
+    {
+        digitalWrite(teensy_start_charge, HIGH);
+    }
+}
+
+void check_charge_condition() {
+    uint16_t cp_v_raw  = analogRead(teensy_cp_v);
+    uint16_t cp_pwm_raw = analogRead(teensy_cp_pwm);
+    uint16_t pp_v_raw  = analogRead(teensy_pp_v);
+    float cp_v_voltage  = (cp_v_raw  * ADC_REF_VOLTAGE) / ADC_MAX_COUNT;
+    float cp_pwm_voltage = (cp_pwm_raw * ADC_REF_VOLTAGE) / ADC_MAX_COUNT;
+    float pp_v_voltage  = (pp_v_raw  * ADC_REF_VOLTAGE) / ADC_MAX_COUNT;
+    
+    //lets check if we are in 240V mode
+    CCUData.level_2_enabled = !(cp_v_voltage <= 0.1); //not in 240V mode
+    
 }
